b3eb84f104e18c65f76edb79e03b6d464607b9c0
MalignusCN <836936333@qq.com>
2022-05-02 17:41:24 +0800

    Find eca_layer is better than original CBAM, replace it.

--- DBED/model/Channel_attention.py
+++ DBED/model/Channel_attention.py
@@ -67,10 +67,10 @@ class CAM_CBAM(nn.Module):
 
 
 class ChannelAttentionModule(nn.Module):
-    def __init__(self, in_planes=4):
+    def __init__(self):
         super(ChannelAttentionModule, self).__init__()
         self.ca_1 = CAM_DANet()
-        self.ca_2 = CAM_CBAM(in_planes)
+        self.ca_2 = eca_layer()
 
     def forward(self, x):
         ca_1 = self.ca_1(x)
@@ -78,3 +78,30 @@ class ChannelAttentionModule(nn.Module):
         feature = ca_1 + ca_2
 
         return feature
+
+
+class eca_layer(nn.Module):
+    """
+    Implementation: 
+    Constructs a ECA module.
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+    """
+    def __init__(self, k_size=3):
+        super(eca_layer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
